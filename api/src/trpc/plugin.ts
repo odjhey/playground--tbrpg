be6fde75383166cb4872d72806c93aedfc9f25d6
odjhey <odjhey@gmail.com>
2023-01-10 10:16:10 +0800

    chore: remove ws

--- api/src/trpc/plugin.ts
+++ api/src/trpc/plugin.ts
@@ -1,6 +1,5 @@
 import { fastifyTRPCPlugin } from "@trpc/server/adapters/fastify";
 import { FastifyInstance } from "fastify";
-import ws from "@fastify/websocket";
 
 import { appRouter } from "./router";
 import { createContext } from "./router/context";
@@ -8,10 +7,8 @@ import { createContext } from "./router/context";
 export default async (fastify: FastifyInstance) => {
   const prefix = "/trpc";
 
-  fastify.register(ws);
   fastify.register(fastifyTRPCPlugin, {
     prefix,
-    useWSS: true,
     trpcOptions: { router: appRouter, createContext },
   });
 };
