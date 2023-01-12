2bb180a036e26f01fb92722f64a3027d69b08c03
odjhey <odjhey@gmail.com>
2023-01-12 15:25:48 +0800

    feat: add component effects

--- api/src/game/components.ts
+++ api/src/game/components.ts
@@ -2,11 +2,12 @@
 
 export const component = <
   State,
-  C extends Record<string, (p: any, s: State) => State>
+  C extends Record<string, (p: any, s: State) => State>,
+  E extends Record<string, [(s: State) => boolean, (s: State) => State]>
 >(def: {
   id: string;
   initState: State;
-  effects: Record<string, (state: State) => void>;
+  effects: E;
   signals: C;
 }) => {
   let state = def.initState;
@@ -17,6 +18,16 @@ export const component = <
       props: Parameters<typeof def.signals[K]>[0]
     ) => {
       state = def.signals[signal](props, state);
+
+      // TODO: effect priority and terminating effects
+      //       maybe a middleware pattern is apt
+      state = Object.keys(def.effects).reduce((accu, k) => {
+        if (def.effects[k][0](accu)) {
+          return def.effects[k][1](accu);
+        }
+        return accu;
+      }, state);
+
       return state;
     },
     state: () => state,
--- api/test/game/components.test.ts
+++ api/test/game/components.test.ts
@@ -5,13 +5,7 @@ test("simple component state", async (t) => {
   const hpComponent = component({
     id: "hp",
     initState: 0,
-    effects: {
-      isDead: (state) => {
-        if (state < 0) {
-          return true;
-        }
-      },
-    },
+    effects: {},
     signals: {
       "+": (a: number, prevState) => {
         return prevState + a;
@@ -41,4 +35,50 @@ test("object component state", async (t) => {
 
   chara.receive("+", 1);
   t.match(chara.state(), { name: "john", hp: 11 });
+});
+
+test("simple component effects - exec", async (t) => {
+  const chara = component({
+    id: "chara",
+    initState: { name: "john", hp: 10, active: true },
+    effects: {
+      die: [
+        (state) => state.hp < 0,
+        (state) => {
+          return { ...state, active: false };
+        },
+      ],
+    },
+    signals: {
+      "-": (a: number, prevState) => {
+        return { ...prevState, hp: prevState.hp - a };
+      },
+    },
+  });
+
+  chara.receive("-", 888);
+  t.match(chara.state(), { name: "john", active: false });
+});
+
+test("simple component effects - not exec", async (t) => {
+  const chara = component({
+    id: "chara",
+    initState: { name: "john", hp: 10, active: true },
+    effects: {
+      die: [
+        (state) => state.hp < 0,
+        (state) => {
+          return { ...state, active: false };
+        },
+      ],
+    },
+    signals: {
+      "-": (a: number, prevState) => {
+        return { ...prevState, hp: prevState.hp - a };
+      },
+    },
+  });
+
+  chara.receive("-", 1);
+  t.match(chara.state(), { name: "john", active: true });
 });
