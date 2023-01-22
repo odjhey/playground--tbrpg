837eb592440d3ca97e3949779d27653bddcbd826
odjhey <odjhey@gmail.com>
2023-01-22 09:40:22 +0800

    feat: sample ping pong long poll

--- api/src/trpc-services/long-poll.ts
+++ api/src/trpc-services/long-poll.ts
@@ -1,6 +1,12 @@
+import { z } from "zod";
 import { publicProcedure, router } from "../trpc/trpc";
 
-const list: string[] = [];
+// move this to some redis or sumthing
+// todo: change to {[match: string]: {values}} for easier indexing
+const list: {
+  req: string;
+  nakatoka: { taken: true; by: string } | { taken: false };
+}[] = [];
 
 export const longPollRouter = router({
   ping: publicProcedure.query(() => {
@@ -27,4 +33,93 @@ export const longPollRouter = router({
       }
     });
   }),
+
+  // NOTE: make sure to add a timeout
+  ping3: publicProcedure
+    .input(z.object({ match: z.string() }))
+
+    // TODO: change to Mutation
+    .query(({ input }) => {
+      // TODO: make sure that input.match is unique
+      list.push({ req: input.match, nakatoka: { taken: false } });
+
+      return new Promise(async (res) => {
+        let ctr = 0;
+
+        // poll every 1 min
+        while (true) {
+          await new Promise((res2) => {
+            setTimeout(() => {
+              if (ctr > 30) {
+                // 30seconds for now
+                res({
+                  result: `naaddaaa`,
+                  values: list,
+                  message: "donzos",
+                });
+              }
+              ctr++;
+
+              const match = list.find((l) => l.req === input.match);
+              if (match && match.nakatoka.taken === true) {
+                res({
+                  result: `yahoo match! ${match.nakatoka.by}`,
+                  values: list,
+                  message: "donzos",
+                });
+              }
+
+              res2({});
+            }, 1000);
+          });
+        }
+      });
+    }),
+
+  // NOTE: make sure to add a timeout
+  pong3: publicProcedure
+    .input(z.object({ match: z.string() }))
+
+    // TODO: change to Mutation
+    .mutation(({ input }) => {
+      return new Promise(async (res) => {
+        let ctr = 0;
+
+        // poll every 1 min
+        while (true) {
+          await new Promise((res2) => {
+            setTimeout(() => {
+              if (ctr > 30) {
+                // 30seconds for now
+                res({
+                  result: `naaddaaa`,
+                  values: list,
+                  message: "donzos",
+                });
+              }
+              ctr++;
+
+              const matchIdx = list.findIndex((l) => l.req === input.match);
+              if (
+                matchIdx !== -1 &&
+                list[matchIdx] &&
+                list[matchIdx].nakatoka.taken === false
+              ) {
+                // TODO: implement own datastructure
+                // TODO: add a cleanup function
+                list[matchIdx].nakatoka = { taken: true, by: input.match };
+
+                res({
+                  result: `yahoo match! ${list[matchIdx].req}`,
+                  values: list,
+                  message: "donzos",
+                });
+              }
+
+              res2({});
+            }, 1000);
+          });
+        }
+      });
+    }),
 });
--- web/src/pages/LongPollPage/LongPollPage.tsx
+++ web/src/pages/LongPollPage/LongPollPage.tsx
@@ -1,13 +1,31 @@
 import { trpc } from "../../utils/trpc";
 
 export const AboutPage = () => {
-  const q = trpc.longPoll.ping.useQuery();
+  const q = trpc.longPoll.ping3.useQuery({ match: "ho12" });
+
+  const match = trpc.longPoll.pong3.useMutation();
 
   return (
     <div>
-      <h1>Long poll</h1>
-      <pre>{JSON.stringify(q.isLoading)}</pre>
-      <pre>{JSON.stringify(q.data)}</pre>
+      <div>
+        <h1>Long poll</h1>
+        <pre>{JSON.stringify(q.isLoading)}</pre>
+        <pre>{JSON.stringify(q.data)}</pre>
+      </div>
+
+      <div>
+        <h1>match</h1>
+        <button
+          className="btn btn-sm"
+          onClick={() => {
+            match.mutate({ match: "ho12" });
+          }}
+        >
+          match
+        </button>
+        <pre>{JSON.stringify(match.isLoading)}</pre>
+        <pre>{JSON.stringify(match.data)}</pre>
+      </div>
     </div>
   );
 };
